2457a37f5b142adc15e6af44e804e48b778f7e4c
ItsoDimitrov <fffsssccc@abv.bg>
2018-12-06 14:51:36 +0200

    Created Sandbox app for testing

--- src/Tests/Sandbox/Program.cs
+++ src/Tests/Sandbox/Program.cs
@@ -1,4 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using AngleSharp.Parser.Html;
+using FMCApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Sandbox
 {
@@ -6,7 +16,42 @@ namespace Sandbox
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"{typeof(Program).Namespace} ({string.Join(" ", args)}) starts working...");
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider(true);
+
+            using (var serviceScope = serviceProvider.CreateScope())
+            {
+                serviceProvider = serviceScope.ServiceProvider;
+                SandboxCode(serviceProvider);
+            }
+        }
+
+        private static void SandboxCode(IServiceProvider serviceProvider)
+        {
+            
+            //var db = serviceProvider.GetService<FMCAppContext>();
+            //Console.WriteLine(db.Users.Count());
+            // TODO : Code here 
+        }
+
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            services.AddDbContext<FMCAppContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
         }
     }
 }
+
+
+
