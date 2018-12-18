aaecf945dc3b7324674af64a5c557f74e6b42bec
ItsoDimitrov <fffsssccc@abv.bg>
2018-12-18 19:15:54 +0200

    Updated to ASP.NET Core 2.2 and EF 2.2 and .NET Core 2.2

--- src/Web/FMCApp.Web/Startup.cs
+++ src/Web/FMCApp.Web/Startup.cs
@@ -54,7 +54,7 @@ namespace FMCApp.Web
                     )
                 .AddEntityFrameworkStores<FMCAppContext>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
