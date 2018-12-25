70c2e4c9f4c82c32d17882673935af34f56a23f1
ItsoDimitrov <fffsssccc@abv.bg>
2018-12-25 17:04:40 +0200

    Added functionality for News - "Read more " button now available

---
+++ src/Web/FMCApp.Web/Controllers/NewsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FMCApp.Data;
+using FMCApp.Web.Models.ViewModels.VisualizationModels.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMCApp.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly FMCAppContext _context;
+
+        public NewsController(FMCAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var newsModel = this._context.Newses.Select(n => new NewsDetails
+            {
+                 Id = n.Id,
+                 Content = n.Content,
+                Title = n.Title
+            }).FirstOrDefault(n => n.Id == id);
+            return View("Details",newsModel);
+        }
+    }
+}
---
+++ src/Web/FMCApp.Web/Models/ViewModels/VisualizationModels/News/NewsDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMCApp.Web.Models.ViewModels.VisualizationModels.News
+{
+    public class NewsDetails
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string HtmlContent => this.Content.Replace("\n", "<br />\n");
+
+    }
+}
