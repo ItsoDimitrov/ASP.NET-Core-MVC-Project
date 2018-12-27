40f049bb6640317b887cd3dd58d2cc7e72e97f11
ItsoDimitrov <fffsssccc@abv.bg>
2018-12-27 18:29:42 +0200

    Implemented Details Movie Action in Movies Controller ( The View is not implemented fully! )

---
+++ src/Web/FMCApp.Web/Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FMCApp.Data;
+using FMCApp.Web.Models.ViewModels.VisualizationModels.Movies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMCApp.Web.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly FMCAppContext _context;
+
+        public MoviesController(FMCAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Details(int? id)
+        {
+            var movieCheck = this._context.Movies.Find(id);
+            if (movieCheck == null)
+            {
+                return NotFound();
+            }
+
+            var movie = this._context.Movies.Select(m => new MovieDetailsViewModel
+            {
+                Id = m.Id,
+                Title = m.Title,
+                Director = m.Director.Name,
+                Description = m.Description,
+                Genre = m.Genre.ToString(),
+                ReleaseDate = m.ReleaseDate,
+                MoviePosterUrl = m.MoviePosterUrl
+                
+            }).FirstOrDefault(m => m.Id == id);
+            return View("Details",movie);
+        }
+    }
+}
---
+++ src/Web/FMCApp.Web/Models/ViewModels/VisualizationModels/Movies/MovieDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMCApp.Web.Models.ViewModels.VisualizationModels.Movies
+{
+    public class MovieDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string MoviePosterUrl { get; set; }
+    }
+}
